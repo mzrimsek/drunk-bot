62104c5ab50e5dacfcca24dcb863aea45f31f2df
Mike Zrimsek <mikezrimsek@gmail.com>
2020-10-31 00:29:21 -0400

    fix parsing of private key with env

--- config/firebase.js
+++ config/firebase.js
@@ -3,7 +3,8 @@ const config = {
     type: "service_account",
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
+    // https://stackoverflow.com/questions/50299329/node-js-firebase-service-account-private-key-wont-parse
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: "https://accounts.google.com/o/oauth2/auth",
