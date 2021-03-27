8f04b2a32e0b9f2ed8282271838deee4582c4306
Mike Zrimsek <mikezrimsek@gmail.com>
2021-03-27 09:40:13 -0400

    remove function

--- src/utils.js
+++ src/utils.js
@@ -15,16 +15,9 @@ function isChannelMessage(message) {
     return message.guild.id !== undefined
 }
 
-function getRandomNumber(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 module.exports = {
     hasRole,
     getUserNameString,
     hasNonTextContent,
-    isChannelMessage,
-    getRandomNumber
+    isChannelMessage
 };
