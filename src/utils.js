61a921b20a0311bd9d2bbda137b8343160b3050d
Mike Zrimsek <mikezrimsek@gmail.com>
2021-03-28 23:38:31 -0400

    remove last unused function

--- src/utils.js
+++ src/utils.js
@@ -2,16 +2,11 @@ function hasRole(member, roleId) {
     return member._roles.includes(roleId);
 }
 
-function getUserNameString(userId) {
-    return `<@!${userId}>`;
-}
-
 function isChannelMessage(message) {
     return message.guild.id !== undefined
 }
 
 module.exports = {
     hasRole,
-    getUserNameString,
     isChannelMessage
 };
